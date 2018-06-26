cb8e26b7b006ba833c51433cca7484c8999c28f1
jsgilberto <alv.mtz94@gmail.com>
2018-06-26 11:39:46 +0200

    added comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
 	validates :title, presence: true, length: {minimum: 20, maximum: 200}
 	validates :content, presence: true, length: {minimum: 100}
 	validates :description, presence: true, length: {maximum: 100}
+	
 	belongs_to :user
+	has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: {maximum: 20, minimum: 4}, format: { without: /\s/ }
   has_many :posts
+  has_many :comments
 end
